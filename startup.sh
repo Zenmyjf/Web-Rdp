0fdcda86ca882ac803be7129679a00212a08fa17
Zenmyjf <114481747+Zenmyjf@users.noreply.github.com>
2024-01-29 19:04:50 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Start noVNC
-/usr/share/novnc/utils/launch.sh --listen 8080 &
-
-# Start x11vnc
-x11vnc -display :1 -rfbport 5901 -noxdamage -forever -bg -nopw
+# Set a password for noVNC
+mkdir -p ~/.vnc
+echo "your_password" | vncpasswd -f > ~/.vnc/passwd
 
 # Start Mate desktop
-mate-session
+mate-session &
+
+# Start x11vnc
+x11vnc -display :1 -rfbport 5901 -noxdamage -forever -bg -usepw
+
+# Start noVNC with password authentication
+websockify -D --web /usr/share/novnc/ --cert /etc/ssl/novnc.pem 6080 localhost:5901
